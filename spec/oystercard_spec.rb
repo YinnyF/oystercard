84bf40485b35f01c1952fd8b44454c6a8c99e813
Masako Gray <masakogray@hotmail.co.uk>
2021-06-01 15:33:25 +0100

    step 3, created Oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,3 +3,19 @@ require 'oystercard'
 describe Oystercard do
   
 end
+
+=begin
+Issue Comments:
+NameError - Raised when a given name is invalid or undefined.
+uninitialized constant Oystercard - 
+./spec/oystercard_spec.rb
+line number - 3
+
+Suggest resolve:
+To create an Oystercard class.
+
+Stack trace:
+- shows the order of the error. 
+- Ruby reads from top to bottom.
+
+=end
